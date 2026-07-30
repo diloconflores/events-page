5394b554c5106a694372c47f264dce3dd6689514
Raymundo Salazar <raymundo.salser@hotmailc.om>
2026-07-29 20:24:08 -0600

    feat(alliances-section): integrate AlliancesSection component and remove OCCASIONS_URL

--- src/components/templates/EventLanding/constants.ts
+++ src/components/templates/EventLanding/constants.ts
@@ -1,5 +1,4 @@
 import { FORMSPREE_ENDPOINT } from "../../../services/forms";
 
-export const OCCASIONS_URL = "https://diloconflores.com";
 export const SHOP_URL = "https://diloconflores.com";
 export const EVENT_FORM_ACTION = FORMSPREE_ENDPOINT;
